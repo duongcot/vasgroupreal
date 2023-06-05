202cf1dd09434dc88baefa8dbb499cb3c765dcdc
duongcot <118062795+duongcot@users.noreply.github.com>
2023-06-05 09:43:26 +0700

    media-tablet

--- main.js
+++ main.js
@@ -1,4 +1,32 @@
-var a = 6;
-var b;
-b = a++ + --a ;
-console.log(b);
+const sports = [
+    {
+        name: 'Bóng rổ',
+        like: 6
+    },
+    {
+        name: 'Bơi lội',
+        like: 5
+    },
+    {
+        name: 'Bóng đá',
+        like: 10
+    },
+]
+
+function getMostFavoriteSport(sports) 
+{
+    var listSports = sports.filter(function (sports){
+         return sports.like > 5 ;
+    })  
+    console.log(listSports);
+}
+
+
+// Kỳ vọng
+console.log(getMostFavoriteSport(sports)) 
+// Output: [{ name: 'Bóng rổ, like: 6 }, { name: 'Bóng đá, like: 10 }]
+
+
+
+
+
